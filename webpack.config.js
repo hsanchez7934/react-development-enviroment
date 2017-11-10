61c62072529192098f32940f71f816e5d6c9a196
Hector Sanchez <hsanchez7934@gmail.com>
2017-11-09 17:17:34 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: __dirname + "/app/index.html",
+  filename: "index.html",
+  inject: "body"
+});
+
+module.exports = {
+  entry: __dirname + "/app/index.js",
+  module: {
+    loaders: [
+      {
+        test: /.js$/,
+        exclude: /node-modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }, output: {
+    filename: "transformed.js",
+    path: __dirname + "/build"
+  },
+  plugins: [HTMLWebpackPluginConfig]
+};
